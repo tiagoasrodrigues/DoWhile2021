9b24b248a2e9fd267e31e6a24186549508468e4e
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-10-19 17:16:11 -0300

    :wrench: chore: creating routes

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,12 @@ app.get("/github", (request, response) => {
   response.redirect(
     `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
   );
-})
+});
+
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+});
 
 app.listen(4000, () => console.log(`🚀 Server is running on PORT 4000`));
