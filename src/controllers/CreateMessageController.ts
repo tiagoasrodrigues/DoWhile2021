11240b339a5332f3c92dd16e669013d349cb2cd4
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-10-20 13:02:35 -0300

    :wrench: chore: setup websocket

--- src/controllers/CreateMessageController.ts
+++ src/controllers/CreateMessageController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { CreateMessageService } from "../services/CreateMessageService";
 
-
 class CreateMessageController {
   async handle(request: Request, response: Response) {
     const { message } = request.body;
